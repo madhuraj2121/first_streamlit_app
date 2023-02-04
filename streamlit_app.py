6161b9abbfbad5d9cb69f8d48dfc7ec8aaee4a55
madhuraj2121 <51529674+madhuraj2121@users.noreply.github.com>
2023-02-04 11:42:31 +0530

    Update streamlit_app.py
    added BF menu and three items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 streamlit.title('my parent new healthy dinner')
-streamlit.header('For the analysis of this problem and the management')
-streamlit.text('of the time spent, please create an incident ticket.')
-
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
