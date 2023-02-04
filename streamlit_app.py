036ecf2230a44812d8ebb9becb6b235bfb7fd47f
madhuraj2121 <51529674+madhuraj2121@users.noreply.github.com>
2023-02-04 11:36:00 +0530

    Update streamlit_app.py
    add comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title('my parent new healthy dinner')
+For the analysis of this problem and the management of the time spent, please create an incident ticket.
+
+Thank you.
