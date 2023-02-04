2242a43065971afc0a9ea2e511315109644d2529
madhuraj2121 <51529674+madhuraj2121@users.noreply.github.com>
2023-02-04 11:38:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title('my parent new healthy dinner')
-For the analysis of this problem and the management of the time spent, please create an incident ticket.
+streamlit.header('For the analysis of this problem and the management')
+streamlit.text('of the time spent, please create an incident ticket.')
 
-Thank you.
